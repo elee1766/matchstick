827653ff7df0019c17680c73a4aad60382152d48
a <a@tuxpa.in>
2026-06-07 00:29:47 -0500

    web: add Cousine font from Google Fonts

--- web/pages/layout.nim
+++ web/pages/layout.nim
@@ -8,6 +8,9 @@ proc layout*(title: string, content: VNode): string =
       meta(charset="utf-8")
       meta(name="viewport", content="width=device-width, initial-scale=1")
       title: text title & " - matchstick"
+      link(rel="preconnect", href="https://fonts.googleapis.com")
+      link(rel="preconnect", href="https://fonts.gstatic.com", crossorigin="")
+      link(rel="stylesheet", href="https://fonts.googleapis.com/css2?family=Cousine:ital,wght@0,400;0,700;1,400;1,700&display=swap")
       link(rel="stylesheet", href="/static/tux.css")
       link(rel="stylesheet", href="/static/style.css")
     body:
